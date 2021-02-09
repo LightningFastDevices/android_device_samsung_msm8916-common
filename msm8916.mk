7f9e90bae12d55b675a8c13fadfb898beca92c4c
DJABhipHop <djabhiphop-DJABHipHop@yahoo.com>
2021-02-09 13:54:42 -0500

    msm8916-common: Move VIBRATOR to device/common
    Since the only devices that don't have VIBRATOR  are wifi only devices then we don't need it here

--- msm8916.mk
+++ msm8916.mk
@@ -382,12 +382,7 @@ PRODUCT_PACKAGES += \
 
 # USB HAL
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
-
-# Vibrator
-PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl \
-    android.hardware.vibrator@1.0-service
+    android.hardware.usb@1.0-service.basic	
 
 # Wifi configuration files
 PRODUCT_COPY_FILES += \
