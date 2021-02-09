ed19126e2be7e96aedfca7d1fc191b4bbef92e48
DJABhipHop <djabhiphop-DJABHipHop@yahoo.com>
2021-02-09 15:25:14 -0500

    msm8916-common: Switch to TARGET_COPY_OUT_VENDOR

--- msm8916.mk
+++ msm8916.mk
@@ -292,7 +292,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
-    frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/handheld_core_hardware.xml
+    frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
 # Power configuration
 PRODUCT_COPY_FILES += \
@@ -382,7 +382,7 @@ PRODUCT_PACKAGES += \
 
 # USB HAL
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic	
+    android.hardware.usb@1.0-service.basic
 
 # Wifi configuration files
 PRODUCT_COPY_FILES += \
